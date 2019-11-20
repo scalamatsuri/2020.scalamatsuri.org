0457c349ce11bbf7685073dfd1bf4990bb0842be
sugoikondo <atsu9615@me.com>
2019-11-21 00:27:46 +0900

    likeボタンじゃなくてシェアボタンだった

--- nuxt_src/components/parts/snsFooter.vue
+++ nuxt_src/components/parts/snsFooter.vue
@@ -3,18 +3,18 @@
   en:
     add_hatena_bookmark: "Add this entry to Hatena bookmark"
     facebook_locale: "en_US"
-    facebook_button_width: "75"
+    facebook_button_width: "82.8"
   ja:
     add_hatena_bookmark: "このエントリーをはてなブックマークに追加"
     facebook_locale: "en_US"
-    facebook_button_width: "97"
+    facebook_button_width: "84.8"
 </i18n>
 
 <template>
   <section class="sns">
     <a class="twitter-share-button sns_item" href="https://twitter.com/intent/tweet">Tweet</a>
     <iframe
-      :src="`https://www.facebook.com/plugins/like.php?locale=${$t('facebook_locale')}&href=https%3A%2F%2Fscalamatsuri.org%2F&width=97&layout=button_count&action=like&size=small&show_faces=true&share=false&height=21&appId`"
+      :src="`https://www.facebook.com/plugins/share_button.php?locale=${$t('facebook_locale')}&href=https%3A%2F%2Fscalamatsuri.org%2F&width=97&layout=button_count&action=like&size=small&show_faces=true&share=false&height=21&appId`"
       :width="$t('facebook_button_width')"
       height="20"
       style="border:none;overflow:hidden"
