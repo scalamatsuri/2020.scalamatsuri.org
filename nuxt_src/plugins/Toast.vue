072d91946f70d8078b51923c3f9d5527f24288ee
sugoikondo <atsu9615@me.com>
2019-12-14 01:03:06 +0900

    やばい、トースト閉じボタン職人が誕生した

--- nuxt_src/plugins/Toast.vue
+++ nuxt_src/plugins/Toast.vue
@@ -1,11 +1,17 @@
 <template>
-  <div v-if="showing" class="toast">
-    <span class="check-icon" />
-    <span class="text">
-      ログインしました
-    </span>
-    <span class="close-btn" />
-  </div>
+  <transition name="top" appear>
+    <div v-if="showing" class="toast">
+      <span class="check-icon" />
+      <span class="text">
+        ログインしました
+      </span>
+      <div class="circle" :style="{ 'animation-duration': `${mergedOption.duration -100}ms` }" @click="close">
+        <div class="circle-inner">
+          ☓
+        </div>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -92,6 +98,97 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.circle {
+    position: relative;
+    width: 22px;
+    height: 22px;
+    background: #C8C8C8;
+    color: #C8C8C8;
+    line-height: 9px;
+    border-radius: 50%;
+    text-align: center;
+    overflow: hidden;
+    z-index: 1;
+    margin-top: 2px;
+    animation-duration: 8s;
+}
+
+.circle::before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: -11px;
+    width: 22px;
+    height: 22px;
+    background: #ffffff;
+    transform-origin: right 11px;
+    z-index: 2;
+    animation: rotate-circle-left 2s linear forwards;
+    animation-duration: inherit;
+}
+
+.circle::after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0px;
+    left: 11px;
+    width: 24px;
+    height: 24px;
+    background: #ffffff;
+    transform-origin: left 11px;
+    z-index: 3;
+    animation: rotate-circle-right 2s linear forwards;
+    animation-duration: inherit;
+}
+
+.circle .circle-inner {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    width: 18px;
+    height: 18px;
+    padding-top: 3.8px;
+    background: #fff;
+    border-radius: 50%;
+    z-index: 4;
+}
+@keyframes rotate-circle-right {
+    0%   {
+        transform: rotate(0deg);
+        background: #ffffff;
+    }
+    50%  {
+        transform: rotate(180deg);
+        background: #ffffff;
+    }
+    50.01% {
+        transform: rotate(360deg);
+        background: #C8C8C8;
+    }
+    100% {
+        transform: rotate(360deg);
+        background: #C8C8C8;
+    }
+}
+
+@keyframes rotate-circle-left {
+    0%   { transform: rotate(0deg); }
+    50%  { transform: rotate(0deg); }
+    100% { transform: rotate(180deg); }
+}
+
+  .top-enter-active, .top-leave-active {
+    transform: translateY(0);
+    transition: transform .3s cubic-bezier(0.360, 0.100, 0.160, 1.000);
+  }
+
+  .top-enter, .top-leave-to {
+    transform: translateY(-100px);
+    will-change: transform;
+  }
+
   .toast {
     position: fixed;
     top: 20px;
