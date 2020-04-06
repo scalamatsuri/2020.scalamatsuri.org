a828b38e6d339937113f578d9afaf51844c91182
sugoikondo <atsu9615@me.com>
2020-04-06 11:03:02 +0900

    プロポーザルを借り追加

---
+++ nuxt_src/components/parts/ListSessionContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="schedule schedule-thin">
+    <div class="schedule_content">
+      <div class="schedule_events">
+        <div v-for="program in programs" :key="program.id" @click="$emit('program-clicked', program)">
+          <table-row :program="program" :locale="$i18n.locale" :on-vote="onVote" :on-unvote="onUnVote" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    programs: {
+      type: Array,
+      required: false,
+      default: () => { return [] }
+    }
+  }
+}
+</script>
