a676b4c8bc1191364efedf13e7cc3fe76382d7a0
sugoikondo <atsu9615@me.com>
2020-04-15 00:10:54 +0900

    transition

--- nuxt_src/components/sections/top/AcceptedSessions.vue
+++ nuxt_src/components/sections/top/AcceptedSessions.vue
@@ -245,4 +245,12 @@ letter-spacing: 0.1em;
     margin-top: 20px;
   }
 }
+
+// animations
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .2s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 </style>
