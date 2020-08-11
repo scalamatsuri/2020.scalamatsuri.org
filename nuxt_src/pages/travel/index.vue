5821cbf6f224a3ac16254cb1a744cfe52847031f
sugoikondo <atsu9615@me.com>
2020-08-11 10:43:53 +0900

    unshow travel page

--- nuxt_src/pages/travel/index.vue
+++ nuxt_src/pages/travel/index.vue
@@ -154,7 +154,10 @@ ja:
 </template>
 
 <script>
+import Page404NotFoundMixin from '@/mixins/page/Page404NotFound.js'
+
 export default {
+  mixins: [Page404NotFoundMixin],
   head() {
     const $t = this.$t.bind(this)
     return {
